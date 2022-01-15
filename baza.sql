9cf921f020f8734013cd9178dc78fdc46df8ebb3
Esma <ezejnilovi1@etf.unsa.ba>
2022-01-15 20:20:10 +0100

    Dodan atribut slika

--- baza.sql
+++ baza.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS "grad" (
 	"naziv"	TEXT,
 	"broj_stanovnika"	INTEGER,
 	"drzava"	INTEGER,
+	"slika"   TEXT,
 	FOREIGN KEY("drzava") REFERENCES "drzava"
 	PRIMARY KEY("id")
 );
@@ -14,11 +15,11 @@ CREATE TABLE IF NOT EXISTS "drzava" (
 	FOREIGN KEY("glavni_grad") REFERENCES "grad"
 	PRIMARY KEY("id")
 );
-INSERT INTO "grad" VALUES (1,'Pariz',2206488,1);
-INSERT INTO "grad" VALUES (2,'London',8825000,2);
-INSERT INTO "grad" VALUES (3,'Beč',1899055,3);
-INSERT INTO "grad" VALUES (4,'Manchester',545500,2);
-INSERT INTO "grad" VALUES (5,'Graz',280200,3);
+INSERT INTO "grad" VALUES (1,'Pariz',2206488,1,'/img/question_mark.jpg');
+INSERT INTO "grad" VALUES (2,'London',8825000,2,'/img/question_mark.jpg');
+INSERT INTO "grad" VALUES (3,'Beč',1899055,3,'/img/question_mark.jpg');
+INSERT INTO "grad" VALUES (4,'Manchester',545500,2,'/img/question_mark.jpg');
+INSERT INTO "grad" VALUES (5,'Graz',280200,3,'/img/question_mark.jpg');
 INSERT INTO "drzava" VALUES (1,'Francuska',1);
 INSERT INTO "drzava" VALUES (2,'Velika Britanija',2);
 INSERT INTO "drzava" VALUES (3,'Austrija',3);
